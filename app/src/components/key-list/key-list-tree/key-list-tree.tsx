8d7774a573c9c5302694a2cf88e6d8e9099bdbe4
ahnuchen <chenchangyu@nuanwa.net>
2022-02-10 20:09:50 +0800

    feat: show right context menu without click event

--- app/src/components/key-list/key-list-tree/key-list-tree.tsx
+++ app/src/components/key-list/key-list-tree/key-list-tree.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import utils from '@/src/common/utils'
-import { Tree } from 'antd'
+import { Menu, Tree } from 'antd'
 import { DataNode, EventDataNode } from 'rc-tree/lib/interface'
 import './key-list-tree.less'
 import { usePersistFn } from 'ahooks'
 import { $bus, EventTypes } from '@/src/common/emitter'
+import { i18n } from '@/src/i18n/i18n'
 
 interface KeyListTreeProps {
   keyList: any[]
@@ -19,6 +20,10 @@ export function KeyListTree({ keyList, client, config }: KeyListTreeProps): JSX.
   const [expandedKeys, setExpandedKeys] = useState<React.Key[]>([])
   const [checkedKeys, setCheckedKeys] = useState<React.Key[]>([])
   const [selectedKeys, setSelectedKeys] = useState<React.Key[]>([])
+  const [contextMenuVisible, setContextMenuVisible] = useState(false)
+  const [cmLeft, setCmLeft] = useState(0)
+  const [cmTop, setCmTop] = useState(0)
+  const [contextNode, setContextNode] = useState<DataNode>()
 
   const onExpand = usePersistFn((expandedKeysValue: React.Key[]) => {
     setExpandedKeys(expandedKeysValue)
@@ -65,14 +70,77 @@ export function KeyListTree({ keyList, client, config }: KeyListTreeProps): JSX.
     }
   })
 
+  const onRightClick = usePersistFn((info: { event: React.MouseEvent; node: EventDataNode }) => {
+    console.log('info.node', info.node)
+    setContextMenuVisible(true)
+    setCmLeft(info.event.clientX)
+    setCmTop(info.event.clientY)
+    setContextNode(info.node)
+  })
+
   useEffect(() => {
     const treeData = separator ? utils.keysToTree(keyList, separator) : utils.keysToList(keyList)
     setTreeData(() => treeData as DataNode[])
   }, [keyList])
 
+  useEffect(() => {
+    if (contextMenuVisible) {
+      document.addEventListener('click', () => {
+        setContextMenuVisible(false)
+      })
+    } else {
+      document.removeEventListener('click', () => {
+        setContextMenuVisible(false)
+      })
+    }
+  }, [contextMenuVisible])
+
+  const contextMenus = [
+    {
+      name: i18n.$t('copy'),
+      onClick() {
+        console.log('copy')
+      },
+    },
+    {
+      name: 'delete',
+      onClick() {
+        console.log('delete')
+      },
+    },
+    {
+      name: i18n.$t('multiple_select'),
+      onClick() {
+        console.log('multiple_select')
+      },
+    },
+    {
+      name: i18n.$t('open_new_tab'),
+      onClick() {
+        console.log('open_new_tab')
+      },
+    },
+  ]
+
+  const contextMenusFold = [
+    {
+      name: i18n.$t('multiple_select'),
+      onClick() {
+        console.log('multiple_select')
+      },
+    },
+    {
+      name: i18n.$t('delete_all'),
+      onClick() {
+        console.log('delete_all')
+      },
+    },
+  ]
+
   return (
     <ul>
       <Tree
+        onRightClick={onRightClick}
         checkable={false}
         virtual={true}
         onExpand={onExpand}
@@ -84,6 +152,15 @@ export function KeyListTree({ keyList, client, config }: KeyListTreeProps): JSX.
         selectedKeys={selectedKeys}
         treeData={treeData}
       />
+      {contextMenuVisible && (
+        <div style={{ left: cmLeft, top: cmTop }} className="context-menu-wrapper">
+          {(contextNode?.children ? contextMenusFold : contextMenus).map((item) => (
+            <div key={item.name} className="context-menu-item" onClick={item.onClick}>
+              {item.name}
+            </div>
+          ))}
+        </div>
+      )}
     </ul>
   )
 }
